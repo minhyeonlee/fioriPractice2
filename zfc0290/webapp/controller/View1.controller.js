97607f692ef1cd319697726658a043e1d18eb9de
MINHYEON LEE <minhyeonlee1@gmail.com>
2024-03-15 06:47:43 +0000

    Location Filter

--- zfc0290/webapp/controller/View1.controller.js
+++ zfc0290/webapp/controller/View1.controller.js
@@ -12,6 +12,12 @@ sap.ui.define([
 
             },
             onSearch: function(){
+                let inpLoc = this.getView().byId("inpLoc").getValue();
+                let oTable = this.getView().byId("idTabClass");
+                // SAP Filter Operator의 함수임(BT, EQ), BT: Between, EQ: Equal
+                 // Filter 연결(1. 값이 모두 있는 상태에서 필터, 2. 필터 후 필터에 적합한 값만 가져옴)
+                let oFilter = new sap.ui.model.Filter("Clocat", "EQ", inpLoc); 
+                oTable.getBinding("items").filter( oFilter );
 
             }
         });
