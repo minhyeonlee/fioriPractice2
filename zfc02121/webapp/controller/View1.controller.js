9b2ef4221b12ef7ab7d08beaf0181c947b264d9b
MINHYEON LEE <minhyeonlee1@gmail.com>
2024-03-19 08:16:14 +0000

    Using Date Type

--- zfc02121/webapp/controller/View1.controller.js
+++ zfc02121/webapp/controller/View1.controller.js
@@ -10,18 +10,16 @@ sap.ui.define([
 
         return Controller.extend("sync04.zfc02121.controller.View1", {
             onInit: function () {
-                // Default Value
-                // let oView = this.getView().byId("inpCondClass");
-                // oView.setValue("400001");
-                // Update Model Setting
                 let oData={
-                    sInfo:{
-                    }
+                    uInfo:{}
                 }
 
                 let oJSONModel = new JSONModel();
                 oJSONModel.setData( oData );
-                this.getView().setModel(oJSONModel, "update")
+                this.getView().setModel(oJSONModel, "update");
+
+                // let oDate =  new Date();
+                // alert(oDate);
             },
             onSearch: function(){
 
@@ -55,9 +53,26 @@ sap.ui.define([
                 let oUpdateModel = this.getView().getModel("update");
                 let oUpdateData = oUpdateModel.getData();
                 let oModel = this.getView().getModel();
+                let oYear = oUpdateData.uInfo.Begda.substr(0,4);
+                let oMonth = oUpdateData.uInfo.Begda.substr(5,2);
+                let oDay = oUpdateData.uInfo.Begda.substr(8,2);
+                let oDate = oYear + oMonth + oDay;
+                alert( oDate);
+                //alert(oYear + oMonth + oDay);
+                // Data (Input의 값을 Direct로 읽어오는경우, Model에 있는 값을 읽어오는 경우)
+                // let valDate = this.getView().byId("inpUpdBegda").getValue();
+                // 1) Input을 Direct로 읽어옴
+                // 2) Model에 Binding된 Data를 읽어옴
+                // alert(valDate);
+                // alert(oUpdateData.uInfo.Begda); //강사님은 이걸 선호함
+                // alert(oUpdateData.uInfo.Begda.substr(0,4));
+
+                
                 let oEntry={
                     "Cname": oUpdateData.uInfo.Cname,
-                    "Clocat": oUpdateData.uInfo.Clocat
+                    "Clocat": oUpdateData.uInfo.Clocat,
+                    //"Begda" : "20240318" // 20240318로 들어감, 20240318로 넣으면 타입이 안맞아서 exception 오류
+                    "BegdaT" : oDate // "2024-03-01" T에 넣고 아밥에서 ls_begda = ls_ztfc0299-begda = ls_ui5-begda_t.
                 };
                 let sPath = oModel.createKey('/esClassSet', {
                     "Class": oUpdateData.uInfo.Class
