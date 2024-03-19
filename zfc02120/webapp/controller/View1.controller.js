0208b1e6e45305ca255deb932a07f4756aa94565
MINHYEON LEE <minhyeonlee1@gmail.com>
2024-03-19 01:58:57 +0000

    Instructor Code 1

--- zfc02120/webapp/controller/View1.controller.js
+++ zfc02120/webapp/controller/View1.controller.js
@@ -10,14 +10,21 @@ sap.ui.define([
         return Controller.extend("sync04.zfc02120.controller.View1", {
             onInit: function () {
                 // Default Value
-                let oView = this.getView().byId("inpClass");
+                let oView = this.getView().byId("inpCondClass");
                 oView.setValue("400001");
             },
             onSearch: function(){
-                let classNo = this.getView().byId("inpClass").getValue();
-                let sPath = "/esClassSet('" + classNo + "')"; 
-                let oPanel = this.getView().byId("idPanel");
-                oPanel.bindElement(sPath);
+                // let classNo = this.getView().byId("inpClass").getValue();
+                // let sPath = "/esClassSet('" + classNo + "')"; 
+                // let oPanel = this.getView().byId("idPanel");
+                // oPanel.bindElement(sPath);
+                // 강사님 코드
+                let oView = this.getView();
+                let inpClassValue = oView.byId("inpCondClass").getValue();
+                // Key가 1개여서 Class=을 생략해도됨.
+                let sPath = "/esClassSet(Class='" + inpClassValue + "')";
+                alert(sPath); // /esClassSet('400001')
+                oView.byId("PanelUpd").bindElement( sPath );
             }
         });
     });
